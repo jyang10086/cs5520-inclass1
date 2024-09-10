eca0653992cf19db6c6d50cf7b80edb1c58373ad
jyang10086 <janewyang10086@gmail.com>
2024-09-10 14:47:45 -0700

    add TextInput component and show what users enter

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
-  const appName = 'Hello5520'
+  const appName = "Hello5520";
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
       <Header appName={appName} />
+      <TextInput
+        style={{ borderBottomColor: "purple", borderBottomWidth: 2 }}
+        autoCorrect={true}
+        keyboardType="default"
+        placeholder="Type here."
+        onChangeText={(newText) => setText(newText)}
+        value={text}
+      />
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
