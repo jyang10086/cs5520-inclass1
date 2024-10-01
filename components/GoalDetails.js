ecd6b4e69019d9257ab8cb46eb82309401db8ad3
jyang10086 <janewyang10086@gmail.com>
2024-10-01 14:31:09 -0700

    navigate to MoreInfo screen when press on more details button

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -1,9 +1,19 @@
-import { Button, View, Text, StyleSheet } from "react-native";
-export default function GoalDetails({route}) {
-  const { item } = route.params;
+import { Button, View, Text } from "react-native";
+export default function GoalDetails({ navigation, route }) {
+  const handleMoreDetails = () => {
+    navigation.push("Details");
+  };
+
   return (
     <View>
-      <Text>Goal Details: {item.text} id: {item.id}</Text>
+      {route.params ? (
+        <Text>
+          Goal Details: {route.params.item.text} id: {route.params.item.id}
+        </Text>
+      ) : (
+        <Text>More Detail</Text>
+      )}
+      <Button title="More Details" onPress={handleMoreDetails} />
     </View>
   );
 }
