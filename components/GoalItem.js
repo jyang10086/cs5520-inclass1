81ffaa12e4bebeb614d7bad3f97618d09bc02501
jyang10086 <janewyang10086@gmail.com>
2024-10-08 13:24:08 -0700

    Remove the ”i” button and use onPress to navigate to new screen

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { Button, View, Text, StyleSheet, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function GoalItem({ item, onDelete }) {
   const navigation = useNavigation();
@@ -9,15 +9,17 @@ export default function GoalItem({ item, onDelete }) {
   };
 
   return (
-    <View key={item.id} style={styles.textView}>
-      <Text style={styles.text}>{item.text}</Text>
-      <View style={styles.buttonView}>
-        <Button title="X" color="grey" onPress={() => onDelete(item.id)} />
+    <Pressable onPress={handlePressInfo}>
+      <View key={item.id} style={styles.textView}>
+        <Text style={styles.text}>{item.text}</Text>
+        <View style={styles.buttonView}>
+          <Button title="X" color="grey" onPress={() => onDelete(item.id)} />
+        </View>
+        {/* <View style={styles.buttonView}>
+          <Button title="i" color="grey" onPress={handlePressInfo} />
+        </View> */}
       </View>
-      <View style={styles.buttonView}>
-        <Button title="i" color="grey" onPress={handlePressInfo} />
-      </View>
-    </View>
+    </Pressable>
   );
 }
 
