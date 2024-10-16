acecc83ae7c64a51395d8d14e4d296674b760722
jyang10086 <janewyang10086@gmail.com>
2024-10-15 21:37:28 -0700

    update warning when loading detail screen

--- components/GoalDetails.js
+++ components/GoalDetails.js
@@ -5,15 +5,14 @@ import PressableButton from "./PressableButton";
 import { updateFromDB } from "../Firebase/firestoreHelper";
 export default function GoalDetails({ navigation, route }) {
   const [textColor, setTextColor] = useState("black");
-
   const handleMoreDetails = () => {
     navigation.push("Details");
   };
 
   const handleWarningPress = () => {
     console.log("warning!");
-    setTextColor("red");
     updateFromDB("goals", route.params.item.id, { warning: true });
+    setTextColor("red");
     navigation.setOptions({ title: "Warning!" });
   };
 
@@ -29,6 +28,13 @@ export default function GoalDetails({ navigation, route }) {
       ),
     });
   }, [navigation]);
+
+  useEffect(() => {
+    if (route.params.item && route.params.item.warning) {
+      setTextColor("red");
+      navigation.setOptions({ title: "Warning!" });
+    }
+  }, [route.params.item]);
 
   return (
     <View>
