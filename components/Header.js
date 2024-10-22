74afce7554525df07288e287f6b0658971e714b8
jyang10086 <janewyang10086@gmail.com>
2024-10-22 16:37:26 -0700

    fix issue

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { View, Text, Dimensions, useWindowDimensions } from "react-native";
+import { View, Text, Dimensions, useWindowDimensions, StyleSheet } from "react-native";
 import React from "react";
 
 const windowWidth = Dimensions.get("window").width;
