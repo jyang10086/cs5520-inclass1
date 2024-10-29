eb8e98074dd090bef6fecb487fa169a0dc8eca3b
jyang10086 <janewyang10086@gmail.com>
2024-10-29 14:39:25 -0700

    separating auth , app screen and add user listener to monitor isUserLoggedIn state

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Firebase/firebaseSetup";
 import Home from "./components/Home";
 import GoalDetails from "./components/GoalDetails";
 import Login from "./components/Login";
@@ -8,27 +11,49 @@ import Signup from "./components/Signup";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user)
+      if (user) {
+        setIsUserLoggedIn(true);
+      } else {
+        setIsUserLoggedIn(false);
+      }
+    });
+
+    return () => unsubscribe(); // Clean up the listener on unmount
+  }, []);
+  const authStack = (
+    <>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+    </>
+  );
+  const appStack = (
+    <>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: "My goals",
+          headerStyle: { backgroundColor: "purple" },
+          headerTintColor: "white",
+        }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={GoalDetails}
+        options={({ route }) => ({
+          title: route.params?.item.text || "More Details",
+        })}
+      />
+    </>
+  );
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={styles.naviContainer}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: "My goals",
-            headerStyle: { backgroundColor: "purple" },
-            headerTintColor: "white",
-          }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={GoalDetails}
-          options={({ route }) => ({
-            title: route.params?.item.text || "More Details",
-          })}
-        />
+        {isUserLoggedIn ? appStack : authStack}
       </Stack.Navigator>
     </NavigationContainer>
   );
