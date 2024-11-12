c451a361a15b96557c44c2f40f86dd2c87faf961
jyang10086 <janewyang10086@gmail.com>
2024-11-12 14:27:21 -0800

    display static map

--- components/LocationManager.js
+++ components/LocationManager.js
@@ -1,8 +1,11 @@
-import React from "react";
-import { View, Button } from "react-native";
+import { useState } from "react";
+import { Alert, View, Button, Image } from "react-native";
 import * as Location from "expo-location";
 
+const MAPS_API_KEY = process.env.EXPO_PUBLIC_mapApikey;
+
 const LocationManager = () => {
+  const [location, setLocation] = useState(null);
   const [response, requestPermission] = Location.useForegroundPermissions();
 
   const verifyPermission = async () => {
@@ -14,24 +17,37 @@ const LocationManager = () => {
     return permissionResponse.granted;
   };
 
+  const mapPreviewUrl = location
+    ? `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${MAPS_API_KEY}`
+    : null;
+
   const locateUserHandler = async () => {
     const hasPermission = await verifyPermission();
 
     if (!hasPermission) {
-      console.log("Location permission not granted.");
+      Alert.alert("Location permission not granted.");
       return;
     }
     try {
-      const location = await Location.getCurrentPositionAsync({});
-      console.log(location)
+      const userLocation = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: userLocation.coords.latitude,
+        longitude: userLocation.coords.longitude,
+      });
     } catch (err) {
-      console.log("get location error:", err);
+      console.log("locate user error:", err);
     }
   };
 
   return (
     <View>
       <Button title="Locate User" onPress={locateUserHandler} />
+      {location && (
+        <Image
+          source={{ uri: mapPreviewUrl }}
+          style={{ width: 400, height: 200 }}
+        />
+      )}
     </View>
   );
 };
